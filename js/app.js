3cbf3ecaad9c8629987414a48dfd261fc92c0238
opilam <dieudonneopilam2@gmail.com>
2022-09-12 04:12:02 +0200

    avant modification de mon input en textarea

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 (function(){
     var msg_envoyes = document.querySelectorAll('#content-users li');
-    var pGauche = document.querySelectorAll('#one-content .msg-content')
-    var DivpGauche = document.querySelector('#one-content .cont')
+    var p = document.querySelectorAll('.msg-content')
     var input = document.querySelector('#input')
-    
-    
+    var btnSearch = document.querySelector('#searchInput');
 
+    var divsearch = document.querySelector('#resultSearch')
+    // creatio de element paragraphe de resultat de recherche
+    var pSearch = document.createElement('p');
 
+    
     for (let index = 0; index < msg_envoyes.length; index++) {
         var msg_envoye = msg_envoyes[index];
         msg_envoye.addEventListener('click',function(){
@@ -30,61 +32,63 @@
         msg_envoye.addEventListener('mouseover',box)
         msg_envoye.addEventListener('mouseout',boxout)
     }
-    console.log(pGauche)
-    var paragraphe = function(){
-        for (let index = 0; index < pGauche.length; index++) {
-            var nP = pGauche[index].innerText;
-        
-        if(DivpGauche.clientWidth>=710){
+
+    var reduire = function(p,tailleEcranUser){
+        var nP = ''
+        if(tailleEcranUser>=710){
             nP =''
             for (let i = 0; i < 80; i++) {
-                nP+=pGauche.innerText[i];
+                nP+=p.innerText[i];
             }
-            return nP+'....'
-        }
-        if(DivpGauche.clientWidth>=400){
+            nP+='....'
+        }else if(tailleEcranUser>=400){
             nP =''
             for (let i = 0; i < 50; i++) {
-                nP+=pGauche.innerText[i];
+                nP+=p.innerText[i];
             }
-            return nP+'....'
-        }
-        if(DivpGauche.clientWidth>=371){
+            nP+='....'
+        }else if(tailleEcranUser>=371){
             nP =''
             for (let i = 0; i < 29; i++) {
-                nP+=pGauche.innerText[i];
+                nP+=p.innerText[i];
             }
-            return nP+'....'
-        }
-        if(DivpGauche.clientWidth>=360){
+            nP+='....'
+        }else if(tailleEcranUser>=360){
             nP =''
             for (let i = 0; i < 29; i++) {
-                nP+=pGauche.innerText[i];
+                nP+=p.innerText[i];
             }
             return nP+'....'
-        }
-        if(DivpGauche.clientWidth>=300){
+        }else if(tailleEcranUser>=300){
             nP =''
             for (let i = 0; i < 24; i++) {
-                nP+=pGauche.innerText[i];
+                nP+=p.innerText[i];
             }
-            return nP+'....'
-        }
-        
-        if(DivpGauche.clientWidth>=200){
+            nP+='....'
+        }else if(tailleEcranUser>=200){
             nP =''
             for (let i = 0; i < 15; i++) {
-                nP+=pGauche.innerText[i];
+                nP+=p.innerText[i];
             }
-            return nP+'....'
+            nP+='....'
         }
-        return nP
+        return nP;
     }
+    var reduireMessageUser = function(lesParagraph){
+        var msgRetour ;
+        
+        for (let i = 0; i < lesParagraph.length; i++) {
+            var paragraphe =lesParagraph[i];
+            var tailleEcranUser = paragraphe.parentElement.clientWidth;
+            
+            msgRetour = reduire(paragraphe,tailleEcranUser);
+            paragraphe.innerText = msgRetour
+        }
     }
-    pGauche.innerHTML = paragraphe()
+
+    reduireMessageUser(p)
 
     input.addEventListener('keyup',function(e){
-        
         var value = input.value
         var textarea = document.createElement('textarea')
         textarea.id="input"
@@ -97,6 +101,10 @@
         
         input.parentElement.replaceChild(textarea,input)
         textarea.focus()
+    })
+
+    btnSearch.addEventListener('keyup',function(){
+        
     })
 
 })()
